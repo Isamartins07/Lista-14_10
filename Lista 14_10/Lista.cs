1cc300f65ffdd171e81752f4d61cc8417a9db953
ISABELLE DA SILVA MARTINS <isabelle.smartins3@senacsp.edu.br>
2024-10-15 22:15:22 -0300

    primeira versão

---
+++ Lista 14_10/Lista.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_14_10
+{
+    internal class Lista
+    {
+        public void Anagrama()//4
+        {
+            // char -- para representar caracteres individuais,seja núm,letra,simbolo...
+            //ToUpper() : Converte todos os caracteres da string para maiúsculos.
+            //ToLower(): Converte todos os caracteres da string para minúsculos.
+
+            char[] letras = new char[5];
+            string palavra;
+            for (int i = 0; i < letras.Length; i++)
+            {
+                Console.Write($"Digite a {i + 1}° letra : ");
+                letras[i] = Convert.ToChar((Console.ReadLine()).ToLower());
+            }
+
+            Console.WriteLine("Digite agora uma palavra com 5 letras");
+            palavra = Console.ReadLine().ToLower();
+
+            char[] palavraArray = palavra.ToCharArray();
+            //Ordena os nomes em ordem alfabética
+            Array.Sort(letras);
+            Array.Sort(palavraArray);
+            //SequenceEqual compara os dois arrays ordenados. 
+            bool saoAnagramas = letras.SequenceEqual(palavraArray);
+
+            Console.WriteLine(saoAnagramas ? "São anagramas" : "Não são anagramas");
+
+
+        }
+
+        public void Repetidos()//3
+        {
+            int[] numeros = new int[10];
+
+            Console.WriteLine("Digite 10 números,pressione Enter para cada número");
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = int.Parse(Console.ReadLine()); 
+            }
+
+            bool temRepetidos = false;
+            for (int i = 0; i < numeros.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numeros.Length; j++)
+                {
+                    if (numeros[i] == numeros[j])
+                    {
+                        temRepetidos = true;
+                        break;
+                    }
+                }
+                
+            }
+
+            if (temRepetidos)
+            {
+                Console.WriteLine("Há números repetidos no vetor.");
+            }
+            else
+            {
+                Console.WriteLine("Não há números repetidos no vetor.");
+            }
+
+        }
+
+        public void Ordenacao()//2
+        {
+            string[] nomes = new string[5]; // Vetor de strings
+
+            Console.WriteLine("Digite 5 nomes, pressione Enter para cada nome");
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                nomes[i] = Console.ReadLine();
+            }
+
+            // Ordena os nomes em ordem alfabética
+            //Array.Sort(nomes);
+
+            // realizar uma ordenação ignorando Letras Maiúsculas ou Minúsculas,  usar StringComparer.OrdinalIgnoreCase com o método Array.Sort.
+            Array.Sort(nomes, StringComparer.OrdinalIgnoreCase);
+
+            Console.WriteLine("\nNomes em ordem alfabética:");
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+
+
+
+        public void Aritmetica()//1
+        {
+            double[] numeros = new double[10]; // vetor double
+            double media, soma = 0;
+
+            Console.WriteLine("Digite 10 números,pressione Enter para cada número");
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = double.Parse(Console.ReadLine());
+
+               media=  soma += numeros[i]/ 10;
+                
+            }
+            Console.WriteLine("Resultado da média "+soma);
+  
+        }
+    }
+}
---
+++ Lista 14_10/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_14_10
+{
+
+    //ToUpper() : Converte todos os caracteres da string para maiúsculos.
+//ToLower(): Converte todos os caracteres da string para minúsculos.
+
+    // Ctrl + K e Ctrl+D arruma o codigo selecionado.
+    //CTRL+K e Ctrl+C deixa em comentário
+    // Console.Clear();limpa a tela
+    //Console.ReadKey(); dar pause no programa para a pessoa ler. congela a tela
+    // menu com switch case
+    // && é "ambos" e OU é  "||"
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string opcao;
+            Lista lista = new Lista();
+
+            //Criando um manual com switch case
+            do
+            {
+
+
+                Console.WriteLine("Escolha uma operação:\n");
+                Console.WriteLine("1 - Média Aritmética");
+                Console.WriteLine("2 - Ordenação Alfabética");
+                Console.WriteLine("3 - Números Repetidos");
+                Console.WriteLine("4 - Anagrama");
+
+                int escolha = Convert.ToInt32(Console.ReadLine());
+
+                switch (escolha)
+                {
+                    // LISTA 
+                    case 1:
+                        lista.Aritmetica();
+                        break;
+
+                        case 2:
+                        lista.Ordenacao();
+                        break;
+
+                        case 3:
+                        lista.Repetidos();
+                        break;
+
+                    case 4:
+                        lista.Anagrama();
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Operação inválida.");
+                        break;
+
+
+                }
+
+                Console.WriteLine("Deseja continuar no programa (S/N)");
+
+
+                opcao = Console.ReadLine().ToUpper(); // to upper serve para ele ler o que o usuário digita independente de estar maiúsculo ou minúsculo
+
+
+
+
+            } while (opcao == "S");
+
+        }
+
+       
+    }
+}
